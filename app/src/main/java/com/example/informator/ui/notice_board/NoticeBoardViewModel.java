63b70d975d7adfb20d46a5a26c1dd5bd5977aca9
Pilkowski <ilsihaterhd@gmail.com>
2019-12-21 13:01:20 +0100

    Przygotowana struktura widoków

---
+++ app/src/main/java/com/example/informator/ui/notice_board/NoticeBoardViewModel.java
@@ -0,0 +1,7 @@
+package com.example.informator.ui.notice_board;
+
+import androidx.lifecycle.ViewModel;
+
+public class NoticeBoardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/informator/ui/timetable/TimetableFragment.java
@@ -0,0 +1,38 @@
+package com.example.informator.ui.timetable;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.informator.R;
+
+public class TimetableFragment extends Fragment {
+
+    private TimetableViewModel mViewModel;
+
+    public static TimetableFragment newInstance() {
+        return new TimetableFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.timetable_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(TimetableViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
