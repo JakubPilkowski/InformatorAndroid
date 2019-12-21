00028fdc13b3c7f921a2db5ea8ee359435fe6e88
Pilkowski <ilsihaterhd@gmail.com>
2019-12-21 12:35:25 +0100

    Wstępny navigator

---
+++ app/src/main/java/com/example/informator/navigation/Navigator.java
@@ -0,0 +1,61 @@
+package com.example.informator.navigation;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class Navigator {
+    FragmentActivity activity;
+
+    public void setActivity(FragmentActivity activity) {
+        this.activity = activity;
+    }
+
+    public void showRozklad(){
+//        activity.getSupportFragmentManager()
+//                .beginTransaction()
+//                .addToBackStack()
+//                .replace()
+//                .setTransition()
+//                .commit();
+    }
+    public void showWeather(){
+//        activity.getSupportFragmentManager()
+//                .beginTransaction()
+//                .addToBackStack()
+//                .replace()
+//                .setTransition()
+//                .commit();
+    }
+    public void showEvents(){
+//        activity.getSupportFragmentManager()
+//                .beginTransaction()
+//                .addToBackStack()
+//                .replace()
+//                .setTransition()
+//                .commit();
+    }
+    public void showOffers(){
+//        activity.getSupportFragmentManager()
+//                .beginTransaction()
+//                .addToBackStack()
+//                .replace()
+//                .setTransition()
+//                .commit();
+    }
+    public void showTimetable(){
+//        activity.getSupportFragmentManager()
+//                .beginTransaction()
+//                .addToBackStack()
+//                .replace()
+//                .setTransition()
+//                .commit();
+    }
+    public void showOffices(){
+//        activity.getSupportFragmentManager()
+//                .beginTransaction()
+//                .addToBackStack()
+//                .replace()
+//                .setTransition()
+//                .commit();
+    }
+
+}
