b1f8df0dcfbceb38a93cf4c15635fcc4ba895d6a
Pilkowski <ilsihaterhd@gmail.com>
2020-02-12 15:25:05 +0100

    Poprawki w wyglądzie tablicy ogłoszeń

--- app/src/main/java/pl/android/informator/adapters/notice_board/NoticeBoardAdapter.java
+++ app/src/main/java/pl/android/informator/adapters/notice_board/NoticeBoardAdapter.java
@@ -4,41 +4,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.informator.R;
-import pl.android.informator.base.BaseHeadAndItemRVAdapter;
-import pl.android.informator.base.BaseViewHolder;
 import com.android.informator.databinding.SingleNoticeBoardBinding;
-import com.android.informator.databinding.SingleNoticeBoardHeaderBinding;
-import pl.android.informator.models.Notice;
-import pl.android.informator.navigation.Navigator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NoticeBoardAdapter extends BaseHeadAndItemRVAdapter<Notice, BaseViewHolder,BaseViewHolder> {
+import pl.android.informator.base.BaseRecyclerViewAdapter;
+import pl.android.informator.base.BaseViewHolder;
+import pl.android.informator.models.Notice;
+import pl.android.informator.navigation.Navigator;
+
+public class NoticeBoardAdapter extends BaseRecyclerViewAdapter<Notice, BaseViewHolder> {
 
 
     private Navigator navigator;
     private List<NoticeBoardAdapterViewModel> viewModels = new ArrayList<>();
     public void setNavigator(Navigator navigator) {
         this.navigator = navigator;
-    }
-
-    @Override
-    public BaseViewHolder onCreateHeaderViewHolder(ViewGroup parent, int viewType, View itemView) {
-        SingleNoticeBoardHeaderBinding binding = SingleNoticeBoardHeaderBinding.bind(itemView);
-        NoticeBoardHeaderAdapterViewModel viewModel = new NoticeBoardHeaderAdapterViewModel();
-        binding.setViewModel(viewModel);
-        return new BaseViewHolder(itemView,binding,viewModel);
-    }
-
-    @Override
-    public int getHeaderLayoutRes() {
-        return R.layout.single_notice_board_header;
-    }
-
-    @Override
-    public void onBindHeaderView(BaseViewHolder holder, int position) {
-        holder.setElement(navigator);
     }
 
     @Override
