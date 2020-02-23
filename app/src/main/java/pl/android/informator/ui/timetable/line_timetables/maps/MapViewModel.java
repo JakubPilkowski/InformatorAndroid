4848be91615ee46db2c5b4baad54b822666bf1c1
Pilkowski <ilsihaterhd@gmail.com>
2020-02-23 15:22:38 +0100

    Zrobiono przykładową mapę

--- app/src/main/java/pl/android/informator/ui/timetable/line_timetables/maps/MapViewModel.java
+++ app/src/main/java/pl/android/informator/ui/timetable/line_timetables/maps/MapViewModel.java
@@ -3,11 +3,14 @@ package pl.android.informator.ui.timetable.line_timetables.maps;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -18,12 +21,18 @@ import com.android.informator.R;
 import com.android.informator.databinding.MapFragmentBinding;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.Polygon;
 import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
 
@@ -34,7 +43,7 @@ import pl.android.informator.activities.MainActivity;
 import pl.android.informator.base.BaseViewModel;
 import pl.android.informator.models.CommunicationLine;
 
-public class MapViewModel extends BaseViewModel implements LocationListener{
+public class MapViewModel extends BaseViewModel implements LocationListener {
     // TODO: Implement the ViewModel4
     public ObservableField<String> lineNumber = new ObservableField<>();
     public ObservableField<String> lineDestination = new ObservableField<>();
@@ -42,7 +51,8 @@ public class MapViewModel extends BaseViewModel implements LocationListener{
     private MapView mapView;
     private LocationManager locationManager;
     private List<OverlayItem> mStartGoalItems = new ArrayList<>();
-    private IMapController mapController ;
+    private IMapController mapController;
+
     public void init(CommunicationLine line) {
         lineNumber.set(String.valueOf(line.getNumber()));
         lineName.set(line.getFrom());
@@ -51,30 +61,22 @@ public class MapViewModel extends BaseViewModel implements LocationListener{
                 getActivity().getApplicationContext()));
 
         mapView = ((MapFragmentBinding) getBinding()).osmMapView;
-//        MyLocationNewOverlay locationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getActivity().getApplicationContext()),mapView);
-//        locationOverlay.enableMyLocation();
-//        mapView.getOverlays().add(locationOverlay);
 
         mapController = mapView.getController();
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setMultiTouchControls(true);
 
-        GeoPoint startPoint = new GeoPoint( 53.78441739158615, 20.498394209024823);
-        GeoPoint endPoint = new GeoPoint(53.80980321476357, 20.43932119204561);
-        //        GeoPoint endPoint = new GeoPoint(53.7799000,20.4941500);
-//        startPoint.distanceToAsDouble(endPoint);
-//        mapController.setCenter(startPoint);
-        OverlayItem startItem = new OverlayItem("", "", startPoint);
-        Drawable newMarker = mapView.getContext().getResources().getDrawable(R.drawable.ic_arrow_forward_black_24dp);
-//        startItem.setMarker(newMarker);
-        mStartGoalItems.add(startItem);
-
-        OverlayItem goalItem = new OverlayItem("", "", endPoint);
-        Drawable newMarker2 = mapView.getContext().getResources().getDrawable(R.drawable.ic_arrow_forward_black_24dp);
+//        GeoPoint startPoint = new GeoPoint( 53.78441739158615, 20.498394209024823);
+//        GeoPoint endPoint = new GeoPoint(53.80980321476357, 20.43932119204561);
+//        OverlayItem startItem = new OverlayItem("", "", startPoint);
+//        Drawable newMarker = mapView.getContext().getResources().getDrawable(R.drawable.ic_arrow_forward_black_24dp);
+////        startItem.setMarker(newMarker);
+//        mStartGoalItems.add(startItem);
+//
+//        OverlayItem goalItem = new OverlayItem("", "", endPoint);
+//        Drawable newMarker2 = mapView.getContext().getResources().getDrawable(R.drawable.ic_arrow_forward_black_24dp);
 //        goalItem.setMarker(newMarker2);
-        mStartGoalItems.add(goalItem);
-
-        //        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+//        mStartGoalItems.add(goalItem);
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
         if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -87,56 +89,79 @@ public class MapViewModel extends BaseViewModel implements LocationListener{
     }
 
     public void initMap() {
-        //mOsmPathOverlay = new OsmPathOverlay(context);
-        //mMapView.getOverlayManager().add(mOsmPathOverlay);
-        Polyline line = new Polyline(mapView);
-        line.setTitle("Central Park, NYC");
-        line.setSubDescription(Polyline.class.getCanonicalName());
-        List<GeoPoint> pts = new ArrayList<>();
-        //here, we create a polygon, note that you need 5 points in order to make a closed polygon (rectangle)
-        GeoPoint startPoint = new GeoPoint( 53.78441739158615, 20.498394209024823);
+
+        final RoadManager roadManager = new OSRMRoadManager(mapView.getContext());
+        roadManager.addRequestOption("routeType=pedestrian");
+        final ArrayList<GeoPoint> pts = new ArrayList<>();
+        GeoPoint startPoint = new GeoPoint(53.78441739158615, 20.498394209024823);
+        GeoPoint point2 = new GeoPoint(53.78346381381083,20.491053279614473);
+        GeoPoint point3 = new GeoPoint(53.782810870608046, 20.488081661273633);
+        GeoPoint point4 = new GeoPoint(53.779488672322515, 20.487450942276684);
+        GeoPoint point5 = new GeoPoint( 53.77793856842446, 20.482316847394653);
+        GeoPoint point6 = new GeoPoint(53.77744089490557,20.478250554638784);
+        GeoPoint point7 = new GeoPoint(53.773567012932666, 20.475327286794833);
+        GeoPoint point8 = new GeoPoint( 53.776128495607566,20.469437043991885);
+        GeoPoint point9 = new GeoPoint(53.77862325096494,20.463359233662185);
         GeoPoint endPoint = new GeoPoint(53.80980321476357, 20.43932119204561);
         pts.add(startPoint);
+        pts.add(point2);
+        pts.add(point3);
+        pts.add(point4);
+        pts.add(point5);
+        pts.add(point6);
+        pts.add(point7);
+        pts.add(point8);
+        pts.add(point9);
         pts.add(endPoint);
-        line.setPoints(pts);
-        line.setGeodesic(true);
-        line.setInfoWindow(new BasicInfoWindow(R.layout.bonuspack_bubble, mapView));
-        //Note, the info window will not show if you set the onclick listener
-        //line can also attach click listeners to the line
-        /*
-        line.setOnClickListener(new Polyline.OnClickListener() {
+
+        new AsyncTask<Void, Void, Road>() {
             @Override
-            public boolean onClick(Polyline polyline, MapView mapView, GeoPoint eventPos) {
-                Toast.makeText(context, "Hello world!", Toast.LENGTH_LONG).show();
-                return false;
+            protected Road doInBackground(Void... voids) {
+                Road road = roadManager.getRoad(pts);
+                return road;
             }
-        });*/
+
+            @Override
+            protected void onPostExecute(Road road) {
+                super.onPostExecute(road);
+                if (road.mStatus != Road.STATUS_OK) {
+                    Log.d("halo", "onPostExecute: " + road.mStatus);
+                }
+                Polyline roadOverlay = RoadManager.buildRoadOverlay(road, getActivity().getResources()
+                        .getColor(R.color.colorGradientStart), 12);
+                roadOverlay.setGeodesic(true);
+                roadOverlay.setPoints(pts);
+                mapView.getOverlays().add(roadOverlay);
+                for (GeoPoint point:pts) {
+                    Marker startMarker = new Marker(mapView);
+                    startMarker.setPosition(point);
+                    startMarker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+                    startMarker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(Marker marker, MapView mapView) {
+                            return true;
+                        }
+                    });
+                    if (point == pts.get(0))
+                        startMarker.setIcon(getActivity().getResources().getDrawable(R.drawable.bus_station_start_marker_orange));
+                    if (point==pts.get(pts.size()-1))
+                        startMarker.setIcon(getActivity().getResources().getDrawable(R.drawable.bus_station_end_marker_orange));
+                    if(point!=pts.get(0) && point!=pts.get(pts.size()-1))
+                        startMarker.setIcon(getActivity().getResources().getDrawable(R.drawable.bus_station_marker_orange));
+                    mapView.getOverlays().add(startMarker);
+                    mapView.invalidate();
+
+                }
+            }
+        }.execute();
+
+
         mapController.setZoom(13.5d);
         mapController.setCenter(startPoint);
-        mapView.getOverlayManager().add(line);
-//        mapView.getOverlays().add(new ItemizedIconOverlay<>(mStartGoalItems,
-//                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-//                    @Override
-//                    public boolean onItemSingleTapUp(int index, OverlayItem item) {
-//                        return true;
-//                    }
-//
-//                    @Override
-//                    public boolean onItemLongPress(int index, OverlayItem item) {
-//                        return false;
-//                    }
-//
-//                }, mapView.getContext()));
-        mapView.invalidate();
     }
 
-    @Override
     public void onLocationChanged(Location location) {
-//        int lat = (int) (location.getLatitude() * 1E6);
-//        int lng = (int) (location.getLongitude() * 1E6);
-//        GeoPoint gpt = new GeoPoint(lat, lng);
-//        mapController.setCenter(gpt);
-//        mMapView.invalidate();
+
     }
 
     @Override
@@ -152,5 +177,8 @@ public class MapViewModel extends BaseViewModel implements LocationListener{
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+    public void onShowLineClick(){
+        getActivity().onBackPressed();
     }
 }
